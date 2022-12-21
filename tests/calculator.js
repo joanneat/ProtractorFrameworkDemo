7efb2c25b9dec5a3a6fa90ad03da748ba0955092
joanneat <joannesalfafara@gmail.com>
2022-12-21 16:51:38 +0800

    Added 1 more test case

--- tests/calculator.js
+++ tests/calculator.js
@@ -25,4 +25,28 @@ describe('demo calculator tests', function(){
 
     });
 
+    it('addition test2', function(){
+
+        //browser.get('http://juliemr.github.io/protractor-demo/');
+        homepage.get('http://juliemr.github.io/protractor-demo/');
+        
+       // element(by.model('first')).sendKeys('2');
+        homepage.enterFirstNumber('4');
+
+        //element(by.model('second')).sendKeys('3');
+        homepage.enterSecondNumber('4')
+
+        //element(by.css('[ng-click="doAddition()"]')).click();
+        homepage.clickGo();
+
+        //let result = element(by.cssContainingText('.ng-binding', '5'));
+        //expect(result.getText()).toEqual('5');
+        homepage.verifyResult('7');
+
+        browser.sleep(4000)
+
+
+    });
+
+
 });
